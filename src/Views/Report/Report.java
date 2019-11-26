7058f8eeef9fa6dd312740572d27bae7721a1ad3
Juan Fernando Parra <jparrax17@gmail.com>
2019-11-27 00:05:43 +1300

    Deleting proof

--- src/Views/Report/Report.java
+++ src/Views/Report/Report.java
@@ -18,9 +18,7 @@ public class Report extends javax.swing.JPanel {
         initComponents();
     }
 
-    
-    //this is a proof
-    
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
