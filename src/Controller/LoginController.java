fb3b9275bb1cff476cd83935fd13bcfb4445d052
PPUARAT <mr.pangseen@gmail.com>
2019-12-02 10:01:39 +1300

    Turn on login

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -18,17 +18,15 @@ public class LoginController {
     public boolean login(String username, char[] password) {
 
         //System.out.println(username +" "+ password);
-
         UserDAO userDAO = new UserDAO();
 
         ArrayList<User> users;
-        return true; // bypass login
-//        users = userDAO.search(username, String.valueOf(password));
-//        if (!users.isEmpty()) {
-//            return true;
-//        }
-//        return false;
-
+//        return true; // bypass login
+        users = userDAO.search(username, String.valueOf(password));
+        if (!users.isEmpty()) {
+            return true;
+        }
+        return false;
 
     }
 }
